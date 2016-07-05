d919471294327d5b1832417e1195db8294838204
Mike Eastes <meastes@nerdery.com>
2016-07-05 08:55:24 -0500

    Added leaderboard component.

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
         </div>
         <player-info></player-info>
         <items></items>
+        <leaderboard></leaderboard>
     </div>
 </template>
 
 <script>
 import Items from './components/Items';
+import Leaderboard from './components/Leaderboard';
 import PlayerInfo from './components/PlayerInfo';
 
 export default {
     components: {
         Items,
+        Leaderboard,
         PlayerInfo,
     },
 };
---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <iframe src="http://thegame.nerderylabs.com"></iframe>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+iframe {
+    border: 0;
+    width: 100%;
+    height: 1400px;
+}
+</style>
